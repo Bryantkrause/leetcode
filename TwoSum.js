69a21f43187d3c80c0fb233eac49a14fa14c69e9
bryantkrause@gmail.com <bryantkrause@gmail.com>
2021-06-09 09:02:17 -0700

    first day

---
+++ TwoSum.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+nums = [2,7,11,15]
+target = 9
+
+var twoSum = function (nums, target) {
+
+    nums.array.forEach(e => {
+        console.log(nums)
+    });
+};
